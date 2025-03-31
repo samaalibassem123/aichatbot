092fd4d3afccb0e4588024edbd330f4aaec0771f
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-03-31 13:52:14 +0100

    c6

--- app/api/confirm/route.ts
+++ app/api/confirm/route.ts
@@ -3,13 +3,11 @@ import { type NextRequest } from 'next/server'
 
 import { createClient } from '@/utils/supabase/server'
 import { notFound, redirect } from 'next/navigation'
-import { error } from 'console'
+
 
 export async function GET(request: NextRequest) {
-  console.log("tessst")
 
   const { searchParams } = new URL(request.url)
-  const ErrorDesc  = searchParams.get('error_description') as string | null
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/'
@@ -24,12 +22,7 @@ export async function GET(request: NextRequest) {
     })  
     if (!error) {
       // redirect user to specified redirect URL or root of app
-      const MailCode = next.split('=')[1];
-      console.log(MailCode)
-      if (MailCode === "access_denied&error_code"){
-        notFound();
-      }
-      redirect('/mailtmp/'+MailCode)
+      redirect(next)
     }
   }
   // redirect the user to an error page with some instructions
