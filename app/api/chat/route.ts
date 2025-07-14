2fb2146f10fd2fff80709013ca46617bc98a7207
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-07-14 11:11:07 +0100

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,19 +1,15 @@
-
-
-import { google } from '@ai-sdk/google';
-import { streamText } from 'ai';
-
+import { google } from "@ai-sdk/google";
+import { streamText } from "ai";
 
 export const maxDuration = 60;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-
-  const result =  streamText({
-
-    model: google('gemini-2.5-pro-exp-03-25'),
-    system: "You are a helpful assistant speaking tunisian language speak only in tunisian. you reply with emojies. try to be funny. if the ask who are u an answer that ur name is Chat_btounsi created by a man named 'Bassem'",
+  const result = streamText({
+    model: google("gemini-2.0-flash-live-001"),
+    system:
+      "You are a helpful assistant speaking tunisian language speak only in tunisian. you reply with emojies. try to be funny. if the ask who are u an answer that ur name is Chat_btounsi created by a man named 'Bassem'",
     messages,
   });
 
